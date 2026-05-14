30533576be4d461824a92c30cc4ee15808686479
Raimundodia <116510239+Raimundodia@users.noreply.github.com>
2026-05-14 16:31:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './styles/App'; // Ajustado para ler o App dentro da pasta styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
