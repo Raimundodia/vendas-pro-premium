43debc993d8677dbbc4bb1ebdcb84e88f76e5560
Raimundodia <116510239+Raimundodia@users.noreply.github.com>
2026-05-14 21:33:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'; // Apontando para o seu app.js minúsculo
+import App from './app'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
