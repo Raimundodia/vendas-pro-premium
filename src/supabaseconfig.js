246af090ba93bb4a273ef19030d7b2b69bf156ef
Raimundodia <116510239+Raimundodia@users.noreply.github.com>
2026-05-14 23:28:44 -0300

    Update Supabase anon key in configuration

--- src/supabaseconfig.js
+++ src/supabaseconfig.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Verifique se as suas chaves estão corretas aqui
 const supabaseUrl = 'SUA_URL_AQUI'; 
-const supabaseAnonKey = 'SUA_KEY_AQUI';
+const supabaseAnonKey = 'SUA_CHAVE_AQUI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
