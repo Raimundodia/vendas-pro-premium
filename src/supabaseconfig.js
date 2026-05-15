99467cb99759d5a2299bcd4664c9b30bc6ab9d86
Raimundodia <116510239+Raimundodia@users.noreply.github.com>
2026-05-14 22:48:36 -0300

    Update Supabase anonymous key placeholder
    Updated the Supabase anonymous key placeholder.

--- src/supabaseconfig.js
+++ src/supabaseconfig.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-client';
 
-// Substitua pelas suas credenciais reais do Supabase
 const supabaseUrl = 'SUA_URL_AQUI';
-const supabaseAnonKey = 'SUA_CHAVE_AQUI';
+const supabaseAnonKey = 'SUA_KEY_AQUI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
