7e85d9f9b63e240c74bc6df58ca5951faea023cc
Raimundodia <116510239+Raimundodia@users.noreply.github.com>
2026-05-14 22:52:34 -0300

    Update supabaseconfig.js

--- src/supabaseconfig.js
+++ src/supabaseconfig.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-client';
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'SUA_URL_AQUI';
+// Substitua com as suas credenciais reais do projeto
+const supabaseUrl = 'SUA_URL_AQUI'; 
 const supabaseAnonKey = 'SUA_KEY_AQUI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
